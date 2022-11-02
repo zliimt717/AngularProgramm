4abaa4b779b2eeb5c4ba03de1dfe8dcf3c9ac6cf
xin.gu <xin.gu@accenture.com>
2022-11-02 18:21:24 +0100

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.compoment';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,6 @@
+
+export class CoursesService{
+    getCourses(){
+        return ["courses1","courses2","courses3"];
+    }
+}
